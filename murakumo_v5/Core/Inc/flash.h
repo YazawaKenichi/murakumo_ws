64751048ad544ca9f9c0b9d4ab6fc852a4927e34
YazawaKenichi <tonfarken83825@gmail.com>
2023-03-02 20:53:29 +0900

    エンコーダの回転数・IMU の慣性データを保存できるようにしている途中

--- murakumo_v5/Core/Inc/flash.h
+++ murakumo_v5/Core/Inc/flash.h
@@ -21,10 +21,26 @@ typedef struct
     uint16_t course_state_count_max;
     uint16_t analogmin[CALIBRATIONSIZE];
     uint16_t analogmax[CALIBRATIONSIZE];
+} AnalogData;
+
+typedef struct
+{
     float speed[COURSE_STATE_SIZE];
     float radius[COURSE_STATE_SIZE];
     float marker[COURSE_STATE_SIZE];
-} FlashBuffer;
+} CourseData;
+
+typedef struct
+{
+    float left[COURSE_STATE_SIZE];
+    float right[COURSE_STATE_SIZE];
+} EncoderData;
+
+typedef struct
+{
+    Vector3 accel;
+    Vector3 gyro;
+} ImuData;
 
 void flash_erase(void);
 void flash_writting(uint32_t, uint8_t*, uint32_t);
@@ -33,7 +49,10 @@ void flash_write();
 void flash_read();
 void flash_init();
 
-extern FlashBuffer flashbuffer;
+extern AnalogData analogdata;
+extern CourseData coursedata;
+extern EncoderData encoderdata;
+extern ImuData;
 
 /* usuage */
 /*
