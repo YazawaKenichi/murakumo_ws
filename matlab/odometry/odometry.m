31b6ed7f871be8b12ccee71e427e00db5854dff7
YazawaKenichi <tonfarken83825@gmail.com>
2024-01-30 04:52:21 +0900

    状態遷移関数による自己位置推定

--- matlab/odometry/odometry.m
+++ matlab/odometry/odometry.m
@@ -7,12 +7,12 @@ filepath = FILEPATH;
 
 % enc %
 imu_filepath = "";
-[x, y] = main(filepath, imu_filepath);
+[x, y, q] = main(filepath, imu_filepath);
 % glaph_plot(x, y);
 
 % imu %
 imu_filepath = IMU_FILEPATH;
-[x, y] = main(filepath, imu_filepath);
+[x, y, q] = main(filepath, imu_filepath);
 glaph_plot(x, y);
 
 legend("enc", "imu", "nan", "nan"); %
@@ -21,44 +21,67 @@ ylabel("y [ m ]");
 
 hold off;
 
-function [x, y] = main(filename, imu_filename)
+function P_t = f(P_t_1, u_t, Dt)
+    x_t_1 = P_t_1(1);
+    y_t_1 = P_t_1(2);
+    t_t_1 = P_t_1(3);
+    v_t = u_t(1);
+    w_t = u_t(2);
+    if abs(w_t) < 1e-10
+        x_t = x_t_1 + v_t * cos(t_t_1) * Dt;
+        y_t = y_t_1 + v_t * sin(t_t_1) * Dt;
+        t_t = t_t_1 + w_t * Dt;
+    else
+        x_t = x_t_1 + v_t * power(w_t, -1) * (   sin(t_t_1 + w_t * Dt) -  sin(t_t_1) );
+        y_t = y_t_1 + v_t * power(w_t, -1) * ( - cos(t_t_1 + w_t * Dt) +  cos(t_t_1) );
+        t_t = t_t_1 + w_t * Dt;
+    end
+    P_t = [x_t, y_t, t_t];
+end
 
+function [x, y, theta] = main(filename, imu_filename)
     use_imu = 1;
     if imu_filename == ""
         use_imu = 0;
     end
-
     % ファイルから左足の速度と右足の速度を取得 %
     datas = readmatrix(filename);
     datas(:, 1);
+    % 結果保存変数 %
     x = zeros([length(datas(:, 1)) 1]);
     y = zeros([length(datas(:, 1)) 1]);
     theta = zeros([length(x) 1]);
+    P = [x, y, theta];
+    % 速度指令値のリスト %
+    vl = datas(:, 2);
+    vr = datas(:, 3);
+    v = (vl + vr) / 2;
     % 計算周期を入力 %
-    sampling_time = 1;
-    for index = 1:length(x) - 1
-        vl = datas(index, 2);
-        vr = datas(index, 3);
-        v = (vl + vr) / 2;
-        omega = 0;
-        if use_imu
-            imu_datas = readmatrix(imu_filename);
-            omega = imu_datas(index, 2) * pi / 180;
-        else
-            tread = 0.1;    % 単位 [ m ] %
-            omega = - (vl - vr) / tread;    % 単位 [ rad / s = ( m / s ) / m ] %
-            for jindex = 1 : length(omega) - 1
-                omega(jindex + 1) = lpf(omega(jindex), omega(jindex + 1), 0.25);
-            end
-            omega
+    Dt = 1;
+    % omega の配列を計算 %
+    if use_imu
+        imu_datas = readmatrix(imu_filename);
+        w = imu_datas(:, 2) * pi / 180
+    else
+        tread = 0.1;    % 単位 [ m ] %
+        w = - (vl - vr) / tread;    % 単位 [ rad / s = ( m / s ) / m ] %
+        % ローパスフィルタを通す
+        for k = 2 : length(w)
+            w(k) = lpf(w(k - 1), w(k), 0.25);
         end
-        theta(index + 1) = theta(index) + omega * sampling_time;
-        % 現在速度と現在角度から二次元座標上の x-y 速度を出す %
-        vx = v * cos(theta(index + 1));
-        vy = v * sin(theta(index + 1));
-        % 積分する %
-        x(index + 1) = x(index) + vx * sampling_time;
-        y(index + 1) = y(index) + vy * sampling_time;
+        w;
+    end
+    % 指令値行列 %
+    u = [v, w];
+    % オドメトリ配列の計算 %
+    for t = 2:length(x)
+        %%% オドメトリの計算 %%%
+        P(t, :) = f(P(t - 1, :), u(t, :), Dt);
+
+        %%% 値の保存 %%%
+        x(t) = P(t, 1);
+        y(t) = P(t, 2);
+        theta(t) = P(t, 3);
     end
 end
 
@@ -94,6 +117,6 @@ function glaph_plot(x, y)
     xlim([xc - half_range, xc + half_range]);
     ylim([yc - half_range, yc + half_range]);
 
-    disp("plot ... OK")
+    disp("plot ... OK");
 end
 
